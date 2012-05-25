a3c31af5911421fc5a782ca3f6ee2a9d33789210
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-05-25 12:01:17 -0400

    Switch lesson 04 to the new API.

---
+++ 04/demo.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+  // create and initialize a 3D renderer
+  var r = new X.renderer3D();
+  r.init();
+  
+  // create a new X.mesh
+  var porsche = new X.mesh();
+  // .. and associate the .stl to it
+  porsche.file = 'http://lessons.goXTK.com/data/porsche.stl';
+  // activate the magic mode which results in a color full rendering since the
+  // point colors are based on the point position
+  porsche.magicmode = true;
+  // set a caption which appears on mouseover
+  porsche.caption = 'The magic Porsche!';
+  
+  // .. add the porsche
+  r.add(porsche);
+  
+  // .. and start the loading/rendering
+  r.render();
+  
+};
