7bfd4351afdfb556b05b6b769dfb11961b99e21c
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-05-25 13:45:53 -0400

    Switch lesson 06 over.

---
+++ 06/demo.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+
+  // create and initialize a 3D renderer
+  var r = new X.renderer3D();
+  r.init();
+  
+  // create a new X.fibers
+  var fibers = new X.fibers();
+  // .. associate the TrackVis .TRK file
+  fibers.file = 'http://lessons.goXTK.com/data/cc_tracks.trk';
+  fibers.caption = 'The Corpus Callosum:<br>connecting the two hemispheres<br>of the human brain.';
+  
+  // .. add the fibers
+  r.add(fibers);
+  
+  // showtime!
+  r.render();
+  
+};
