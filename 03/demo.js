d2272318167b662fb1818989d8956a15f53d52d9
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-05-25 11:50:37 -0400

    Convert lesson 03 to the new API.

---
+++ 03/demo.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+
+  // create and initialize a 3D renderer
+  var r = new X.renderer3D();
+  r.init();
+  
+  // create a bunch of cubes (1000 to be accurate)
+  // display them in a grid of 20 rows and 50 columns
+  for ( var y = 0; y < 20; y++) {
+    for ( var x = 0; x < 50; x++) {
+      
+      // a new cube
+      var c = new X.cube();
+      // set the center position in world space
+      c.center = [x * 3, y * 3, 0];
+      // configure the edge length
+      c.lengthX = c.lengthY = c.lengthZ = 2;
+      // .. and the color based on the
+      c.color = [150 % x, 150 % y, 1];
+      
+      // add it
+      r.add(c);
+      
+    }
+  }
+  
+  r.render();
+  
+  // .. and animate!!
+  setInterval(function() {
+
+    // rotate the camera rin X-direction (which triggers re-rendering)
+    r.camera.rotate([1, 0]);
+    
+  }, 16.7); // best value if requestAnimationFrame is not used
+  
+};
