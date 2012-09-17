81f7c912d199ce01d6d45c94f880c347e39e4caa
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-09-16 23:41:41 -0400

    Some tests regarding animations.

--- 16/demo.js
+++ 16/demo.js
@@ -1,89 +1,49 @@
 window.onload = function() {
 
-  
-  // create a bunch of X.volumes
-  // each holds a data set
-  _volumes = [];
-  for ( var i = 24; i < 26; i++) {
+  // create an X.animation
+  animation = new X.animation();
+  // animate every 20 frames
+  animation.speed = 20;
+
+    // attach a bunch of X.volumes
+  for ( var i = 24; i < 28; i++) {
     
     var _v = new X.volume();
     _v.file = 'http://x.babymri.org/?' + i + '.mgh';
-    _volumes.push(_v);
     
-    // activate volume rendering for each volume
+    // and configure each of them
     _v.lowerThreshold = 80;
     _v.windowLower = 115;
     _v.windowHigh = 360;
     _v.minColor = [0, 0.06666666666666667, 1];
     _v.maxColor = [0.5843137254901961, 1, 0];
     _v.opacity = 0.2;
-    _v.volumeRendering = true;
-    //v.modified();
+    _v.volumeRendering = true;  
+    _v.caption = i + ' weeks';
     
-    // and hide it by default
-    //v.visible = false;
-    _v.Ha = false;    
+    // finally add it to the animation
+    animation.add(_v);
     
   }
-  
+
   // create a new renderer
   var r = new X.renderer3D();
   r.init();
   
-  for (v in _volumes) {
-    
-    _volumes[v].borders = false;
-    r.add(_volumes[v]);
-    
-  }
+  // add the animation
+  r.add(animation);
   
-  // .. and render it
+  // adjust the camera
+  r.camera.position = [0,200,0];
+  
+  // .. and render the animation
   r.render();
   
-  r.onShowtime = function() {
-
-    for (v in _volumes) {
-      
-      v = _volumes[v];
-      
-
-      
-    }
-    
-  };
-  
-  var _currentVolume = 0;
-  
-  var count = 0;
-  
   r.onRender = function() {
-
-    if (count % 100) {
-      
-      if (_currentVolume >= 1) {
-        
-        //_volumes[_currentVolume - 1].visible = false;
-        _volumes[_currentVolume - 1].Ha = false;
-        
-      }
-      
-      if (_currentVolume > _volumes.length - 1) {
-        
-        _currentVolume = 0;
-        
-      }
-      
-      //_volumes[_currentVolume].visible = true;
-      _volumes[_currentVolume].Ha = true;
-      
-      _currentVolume++;
-      
-    }
     
-    count++;
+    // grab the currently shown object's caption
+    document.getElementById('label').innerHTML = animation.currentObject.caption;
     
   };
   
-
-
 };
