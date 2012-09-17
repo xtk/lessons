85c7bcb952487dd7a500cd65fa09723373879b4d
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-09-17 17:59:47 -0400

    More work on lesson 16.

--- 16/demo.js
+++ 16/demo.js
@@ -1,11 +1,15 @@
+goog.require('X.renderer3D');
+goog.require('X.animation');
+goog.require('X.volume');
+
 window.onload = function() {
 
   // create an X.animation
   animation = new X.animation();
   // animate every 20 frames
   animation.speed = 20;
-
-    // attach a bunch of X.volumes
+  
+  // attach a bunch of X.volumes
   for ( var i = 24; i < 28; i++) {
     
     var _v = new X.volume();
@@ -18,14 +22,14 @@ window.onload = function() {
     _v.minColor = [0, 0.06666666666666667, 1];
     _v.maxColor = [0.5843137254901961, 1, 0];
     _v.opacity = 0.2;
-    _v.volumeRendering = true;  
+    _v.volumeRendering = true;
     _v.caption = i + ' weeks';
     
     // finally add it to the animation
     animation.add(_v);
     
   }
-
+  
   // create a new renderer
   var r = new X.renderer3D();
   r.init();
@@ -34,15 +38,40 @@ window.onload = function() {
   r.add(animation);
   
   // adjust the camera
-  r.camera.position = [0,200,0];
+  r.camera.position = [0, 0, 250];
   
   // .. and render the animation
   r.render();
   
   r.onRender = function() {
-    
+
     // grab the currently shown object's caption
-    document.getElementById('label').innerHTML = animation.currentObject.caption;
+    if (animation.currentObject) {
+      document.getElementById('label').innerHTML = animation.currentObject.caption;
+    }
+    
+  };
+  
+  r.onShowtime = function() {
+
+    var preferences = {
+      'opacity': 0.2
+    };
+    
+    var gui = new dat.GUI();
+    var speedController = gui.add(animation, 'speed', 1, 30);
+    var opacityController = gui.add(preferences, 'opacity', 0, 1);
+    gui.open();
+    
+    opacityController.onChange(function(value) {
+
+      for (v in animation.children) {
+        
+        animation.children[v].opacity = value;
+        
+      }
+      
+    });
     
   };
   
