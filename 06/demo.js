a1f02790375ef1aeed1a214f946662bf53dc74c9
Nicolas Rannou <huko.rannou@gmail.com>
2013-08-16 09:11:37 -0400

    update to support new API

--- 06/demo.js
+++ 06/demo.js
@@ -3,6 +3,7 @@ window.onload = function() {
   // create and initialize a 3D renderer
   var r = new X.renderer3D();
   r.init();
+  r.camera.position = [0, 0, 150];
   
   // create a new X.fibers
   var fibers = new X.fibers();
